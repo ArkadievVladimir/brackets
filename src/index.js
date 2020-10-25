ebb3a10fc05dfbf9d011325a22db25a643d2a722
arkadievvladimir <arkadiev.vladimir@gmail.com>
2020-10-25 21:44:49 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
+
+
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let bracketsType = [],
+  step = str.length / 2;
+
+  bracketsConfig.forEach(elem => bracketsType.push(elem[0] + elem[1]));
+
+  for (let i = 0; i < step; i++) {
+    bracketsType.forEach(elem => str = str.replace(elem, ''));
+    if (str.length === 0) return true;
+  };
+
+  return false
 }
+
